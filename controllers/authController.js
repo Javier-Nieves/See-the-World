ad5e9fbe357117c21e40e9d549f108816e03ed1c
Javier Nieves <javier.nikolskiy@gmail.com>
2023-10-12 09:09:37 -0500

    login system

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,27 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const appError = require('../utils/appError');
 
-exports.signup = async (req, res, next) => {
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  user.password = undefined;
+  res.cookie('jwt', token, cookieOptions);
+  res.status(statusCode).json({ status: 'success', token, data: { user } });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -9,4 +30,19 @@ exports.signup = async (req, res, next) => {
   });
 
   res.status(201).json({ status: 'success', data: { newUser } });
-};
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //   if (!email || !password)
+  //     return next('Please provide email and password', 400);
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    // todo - global error handler
+    console.log('no such user');
+    return next();
+  }
+
+  createSendToken(user, 200, res);
+});
